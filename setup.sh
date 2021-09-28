ef0601daf56ca75739e6c8e5816da429e441d5cb
ggalloro <galloro.giovanni@gmail.com>
2021-09-28 13:18:33 +0200

    Made setup.sh work on both Linux and Mac

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "Enter 1st user name:"
 read user1
-sed -i "s/user1/$user1/g" assets/namespaces/application1/user1_bind.yaml
+sed -i.bak "s/user1/$user1/g" assets/namespaces/application1/user1_bind.yaml
 echo "Enter 2nd user name:"
 read user2
-sed -i "s/user2/$user2/g" assets/namespaces/application2/user2_bind.yaml
+sed -i.bak "s/user2/$user2/g" assets/namespaces/application2/user2_bind.yaml
+rm -rf assets/namespaces/application1/user1_bind.yaml.bak
+rm -rf assets/namespaces/application2/user2_bind.yaml.bak
--- setup2.sh
+++ setup2.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 echo "Enter 1st user name:"
 read user1
-sed -i.bak "s/user1/$user1/g" assets/namespaces/application1/user1_bind.yaml
+sed -i "s/user1/$user1/g" assets/namespaces/application1/user1_bind.yaml
 echo "Enter 2nd user name:"
 read user2
-sed -i.bak "s/user2/$user2/g" assets/namespaces/application2/user2_bind.yaml
-rm -rf assets/namespaces/application1/user1_bind.yaml.bak
-rm -rf assets/namespaces/application2/user2_bind.yaml.bak
+sed -i "s/user2/$user2/g" assets/namespaces/application2/user2_bind.yaml
