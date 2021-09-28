d83177d08ca06ea39b6dca16eb1f6fbaf96a29a2
ggalloro <galloro.giovanni@gmail.com>
2021-09-28 13:11:36 +0200

    Trying a setup script that works on mac & linux

---
+++ setup2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Enter 1st user name:"
+read user1
+sed -i.bak "s/user1/$user1/g" assets/namespaces/application1/user1_bind.yaml
+echo "Enter 2nd user name:"
+read user2
+sed -i.bak "s/user2/$user2/g" assets/namespaces/application2/user2_bind.yaml
+rm -rf assets/namespaces/application1/user1_bind.yaml.bak
+rm -rf assets/namespaces/application2/user2_bind.yaml.bak
