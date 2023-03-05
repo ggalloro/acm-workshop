09496ca825dba3e86742c4f2ff3a997ae6a440ec
ggalloro <galloro.giovanni@gmail.com>
2023-03-05 21:37:06 +0100

    Migrating

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ echo "ACM Workshop user accounts set up"
 echo
 echo "Enter 1st user name:"
 read user1
-sed -i.bak "s/user1/$user1/g" assets/namespaces/application1/rolebinding_user1yaml
+sed -i.bak "s/user1/$user1/g" assets/namespaces/application1/rolebinding_user1.yaml
 echo "Enter 2nd user name:"
 read user2
 sed -i.bak "s/user2/$user2/g" assets/namespaces/application2/rolebinding_user2.yaml
